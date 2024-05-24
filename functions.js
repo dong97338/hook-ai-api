62e136968105898beccc3d2ac4510c5974416f5b
Donghyeon Leee <jdong97338@gmail.com>
2024-05-24 13:33:47 +0000

    feat: add keyword feature

--- functions.js
+++ functions.js
@@ -2,121 +2,96 @@ import axios from 'axios'
 import cheerio from 'cheerio'
 import {v4 as uuidv4} from 'uuid'
 import OpenAIApi from 'openai'
-
 import dotenv from 'dotenv'
 
 dotenv.config()
-
 const openai = new OpenAIApi({key: process.env.OPENAI_API_KEY})
-
-const useGPT = 0
+const useGPT = 1
 
 export async function fetchAndExtractText(url) {
   try {
-    console.log(`Fetching data from ${url}`)
     const response = await axios.get(url)
     const $ = cheerio.load(response.data)
     $('script, style, head, title, meta').remove()
-    const text = $('body').text().trim()
-    console.log(`Extracted text from ${url}`)
-    return text
+    return $('body').text().trim()
   } catch (error) {
-    console.error(`Error fetching data from ${url}: ${error}`)
+    console.error(`Error: ${error}`)
     return ''
   }
 }
 
 export async function processText(text) {
-  console.log('text', text)
-  console.log('Generating title for extracted text.')
-  const title = useGPT ? await generateTitle(text) : 'Generated Title'
-  console.log('Title generated.')
-
-  console.log('Summarizing the text.')
-  const summary = useGPT ? await generateSummary(text, title) : 'Generated Summary'
-  console.log('Summary generated.')
-
-
+  const {title, summary} = useGPT ? await generateTitleAndSummary(text.slice(0, 10000)) : {title: 'Generated Title', summary: 'Generated Summary'}
+  const keywords = useGPT ? await generateKeyWords(text.slice(0, 10000)) : ['키워드1', '키워드2', '키워드3']
+  console.log(keywords)
   const chunks = splitTextIntoChunks(text, 512)
-  console.log('chunks', chunks)
-  const vectors = await Promise.all(
-    chunks.map(async (chunk, index) => {
-      console.log(`Processing chunk ${index + 1} of ${chunks.length}`)
-      return processChunk(chunk, title, summary)
-    })
-  )
-
-  console.log('vector', vectors)
-
-  console.log('Text processing completed for one chunk.')
-  return {title, summary, vectors}
+  const vectors = await Promise.all(chunks.map((chunk, i) => processChunk(chunk, title, summary, i, chunks.length)))
+  return {title, keywords, summary, vectors}
 }
 
-async function processChunk(chunk, title, summary) {
+async function processChunk(chunk, title, summary, index, total) {
+  console.log(`Processing chunk ${index + 1} of ${total}`)
   const embeddingResponse = await openai.embeddings.create({
     model: 'text-embedding-3-small',
     input: `${title}\n${summary}\n${chunk}`
   })
-  return {
-    id: uuidv4(),
-    values: embeddingResponse.data[0].embedding,
-    metadata: {title, summary, chunk}
-  }
+  return {id: uuidv4(), values: embeddingResponse.data[0].embedding, metadata: {title, summary, chunk}}
 }
 
 function splitTextIntoChunks(text, size) {
-  let chunks = []
-  for (let i = 0; i < text.length; i += size) {
-    chunks.push(text.slice(i, i + size))
-  }
-  return chunks
+  return Array.from({length: Math.ceil(text.length / size)}, (_, i) => text.slice(i * size, (i + 1) * size))
 }
 
-async function generateTitle(text) {
+async function generateTitleAndSummary(text) {
+  // Generate the title
   const titleResponse = await openai.chat.completions.create({
-    model: 'gpt-4-turbo',
+    model: 'gpt-3.5-turbo-0125',
     messages: [
-      {
-        role: 'system',
-        content: '너는 웹페이지의 제목을 만드는 봇이야. 내용을 읽고 제목을 만들어줘'
-      },
-      {
-        role: 'user',
-        content: `[content]${text}`
-      }
+      {role: 'system', content: '너는 웹페이지의 제목을 만드는 봇이야. 내용을 읽고 제목을 만들어줘'},
+      {role: 'user', content: `[content]${text}`}
     ],
     temperature: 0,
     max_tokens: 4095
   })
-  return titleResponse.choices[0].message.content.trim()
+
+  const title = titleResponse.choices[0].message.content.trim()
+
+  // Generate the summary using the title
+  const summaryResponse = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-0125',
+    messages: [
+      {role: 'system', content: '너는 웹페이지를 요약하는 봇이야. 내용을 하나의 긴 문단으로 요약해줘'},
+      {role: 'user', content: `[title]${title}\n[content]${text}`}
+    ],
+    temperature: 0,
+    max_tokens: 4095
+  })
+
+  const summary = summaryResponse.choices[0].message.content.trim()
+
+  // Return both title and summary
+  console.log(title, summary)
+  return {title, summary}
 }
 
-async function generateSummary(text, title) {
-  const summaryResponse = await openai.chat.completions.create({
-    model: 'gpt-4-turbo',
+//주어진 텍스트로부터 키워드 3개를 리스트로 반환
+async function generateKeyWords(text) {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-0125',
     messages: [
-      {
-        role: 'system',
-        content: '너는 웹페이지를 요약하는 봇이야. 내용을 하나의 긴 문단으로 요약해줘'
-      },
-      {
-        role: 'user',
-        content: `[title]${title}\n[content]${text}`
-      }
-    ],
-    temperature: 0,
-    max_tokens: 4095
+      {role: 'system', content: '너는 텍스트로부터 키워드를 추출하는 봇이야. 텍스트를 읽고 키워드를 3개 추출하고 쉼표로 구분해줘.'},
+      {role: 'user', content: `[content]${text}`}
+    ]
   })
-
-  return summaryResponse.choices[0].message.content.trim()
+  return response.choices[0].message.content.split(',').map(keyword => keyword.trim())
 }
 
 export async function getEmbeddings(text) {
-  const embeddingResponse = await openai.embeddings.create({
+  const response = await openai.embeddings.create({
     model: 'text-embedding-3-small',
     input: text
   })
-  return embeddingResponse.data[0].embedding
+  return response.data[0].embedding
 }
 
 export async function generateResponse(messages) {
